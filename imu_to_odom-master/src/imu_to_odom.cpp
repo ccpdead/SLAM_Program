18cd5646bf1dc33e293f8b624094153c3c43d46a
yeolume <weixinyii386@163.com>
2022-12-05 17:52:04 +0800

    add imu_odom

---
+++ imu_to_odom-master/src/imu_to_odom.cpp
@@ -0,0 +1,13 @@
+#include <imu_to_odom/imu_to_odom.hpp>
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "imu_to_odom");
+
+  ros::NodeHandle nh;
+
+  ImuOdom *imu_to_odom = new ImuOdom(nh);
+  
+  ros::spin();
+
+  return 0;
+}
